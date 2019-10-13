1ff6bdda82b6eb72377e825c31b5ee6c4a7a6ff5
Ubuntu <ubuntu@ip-172-31-39-21.eu-west-1.compute.internal>
2019-10-13 12:08:55 +0000

    models updated

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Repo(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+#   templates = models.CharField(max_length=200)
+    webhook_key = models.CharField(max_length=50)
+    
+class RepoVersion(models.Model):
+    tag = models.ForeignKey(Repo, on_delete=models.CASCADE)
+    #working_dir =  
+
+class Template(models.Model):
+    name = models.CharField(max_length=200)
+    path = models.FileField(upload_to=None)
+    repo_version = models.ForeignKey(Repo, on_delete=models.CASCADE)
+    
+class TemplateVars(models.Model):
+    name = models.CharField(max_length=200)
+    path = models.FileField(upload_to=None)
+    parent_template = models.ForeignKey(Template, on_delete=models.CASCADE)
